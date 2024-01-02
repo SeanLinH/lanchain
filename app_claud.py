377caebed6dcbde49e95b29d9cf82c9cc4d0c320
Sean Shih-Huan Lin <sean.lin040@gmail.com>
2024-01-02 13:02:52 +0800

    update

--- app_claud.py
+++ app_claud.py
@@ -1,22 +1,44 @@
-from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
+from anthropic import AsyncAnthropic, HUMAN_PROMPT, AI_PROMPT
 from dotenv import load_dotenv
 import os 
 import streamlit as st
+import asyncio
 
 load_dotenv()
 
-anthropic = Anthropic(
+anthropic = AsyncAnthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=os.getenv("API_CLAUD"),
 )
 
-prompt = st.text_input('士桓AI哥在此')
 
-if prompt:
-    # AI_PROMPT = "My name is Sean (林士桓), a dedicated AI engineer from Taiwan, currently serving in a PCB manufacturing. I possess profound expertise and passion in AI, encompassing training, deployment, and application. My competencies extend to image processing, object recognition, YOLO, deep learning, machine learning, OpenCV, PyTorch, CNN, feature extraction, image segmentation, and model fine-tuning. As a 30-year-old professional, I am not only enthusiastic about embracing new challenges but also consistently expanding my network. Currently, I am actively seeking opportunities to transition to an overseas company, aspiring to further enhance my professional capabilities and career progression."
-    completion = anthropic.completions.create(
+
+#######
+
+async def generate_text(x):
+    container = st.empty()
+    ai_prompt = "You are a professional AI expert. If I ask you question related to math, AI, DS, DL, ML, you can answer them from a professional perspective and give me career interview suggestions. You can choose to search online to get more accurate information.  If you feel that the question I asked may not be so important, or there are other more important questions that I may not understand, you can try to guide me to further understand the relevant technical knowledge."
+    text = ""
+    stream = await anthropic.completions.create(
         model="claude-2.1",
         max_tokens_to_sample=300,
-        prompt=f"{HUMAN_PROMPT} {prompt} {AI_PROMPT}",
+        prompt=f"{HUMAN_PROMPT} {x} {AI_PROMPT} {ai_prompt}",
+        stream=True,
+        temperature=0.9,
     )
-    st.write(completion.completion)
+    # print(completion.completion)
+    async for completion in stream:
+        new_char = completion.completion
+        text += new_char
+        container.text(text)
+        asyncio.sleep(0.1)  # 等待一小段時間
+        # print(completion.completion, end="", flush=True)
+
+prompt = st.text_input('士桓AI哥在此')
+
+# Streamlit 按鈕來控制生成
+# if st.button('Start Generation'):
+#     asyncio.run(generate_text())
+
+if prompt:
+    asyncio.run(generate_text(prompt))
